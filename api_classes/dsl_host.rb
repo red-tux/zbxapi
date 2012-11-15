d6da97220951d482f0967b59cff59a14c2ed5a9d
Brett Lentz <blentz@redhat.com>
2012-11-15 15:20:34 -0500

    add selectInterfaces as valid param for dsl_host.

--- api_classes/dsl_host.rb
+++ api_classes/dsl_host.rb
@@ -18,7 +18,7 @@ class Host < ZabbixAPI_Base
                              "editable","filter","search","startSearch","excludeSearch",
                              "searchWildcardsEnabled","output","select_groups","selectParentTemplates",
                              "select_items","select_triggers","select_graphs","select_applications",
-                             "select_macros","select_profile","countOutput","groupOutput",
+                             "selectInterfaces","select_macros","select_profile","countOutput","groupOutput",
                              "preservekeys","sortfield","sortorder","limit","extendoutput"]
   end
 
