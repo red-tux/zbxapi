e38122b2cd0e166e899a6dc1118b57fe6443a969
Derek So <derekso.email@gmail.com>
2012-11-21 19:57:03 +0800

    Add interfaces property for host which is supported in zabbix 2.0.3

--- api_classes/dsl_host.rb
+++ api_classes/dsl_host.rb
@@ -30,7 +30,7 @@ class Host < ZabbixAPI_Base
     add_valid_params "1.3", ["host","port","status","useip",
         "dns","ip","proxy_hostid","useipmi","ipmi_ip","ipmi_port",
         "ipmi_authtype","ipmi_privilege","ipmi_username",
-        "ipmi_password","groups","templates"]
+        "ipmi_password","groups","templates","interfaces"]
   end
 
   action :delete do
