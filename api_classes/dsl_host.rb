e6d39d934a2deecc5885308cb9990fa30b692afc
redtux <nelsonab@red-tux.net>
2012-12-07 09:10:45 -0500

    Moved Interfaces parameter for create method to a separate list for Zabbix 2.0

--- api_classes/dsl_host.rb
+++ api_classes/dsl_host.rb
@@ -42,9 +42,13 @@ class Host < ZabbixAPI_Base
 
   action :create do
     add_valid_params "1.3", ["host","name","port","status","useip",
-        "dns","ip","proxy_hostid","useipmi","ipmi_ip","ipmi_port",
-        "ipmi_authtype","ipmi_privilege","ipmi_username",
-        "ipmi_password","groups","templates","interfaces"]
+                             "dns","ip","proxy_hostid","useipmi","ipmi_ip","ipmi_port",
+                             "ipmi_authtype","ipmi_privilege","ipmi_username",
+                             "ipmi_password","groups","templates"]
+    add_valid_params "2.0", ["host","name","port","status","useip",
+                             "dns","ip","proxy_hostid","useipmi","ipmi_ip","ipmi_port",
+                             "ipmi_authtype","ipmi_privilege","ipmi_username",
+                             "ipmi_password","groups","templates","interfaces"]
   end
 
   action :delete do
